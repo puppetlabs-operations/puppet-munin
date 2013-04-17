a09ff44f26fa082aedcc1f9adcb5f091473fab3e
Zach Leslie <zach@puppetlabs.com>
2013-04-17 11:35:15 -0700

    scrap that, we should be able to take an array for $plugin, and magic should happen

--- manifests/plugin/snmp.pp
+++ manifests/plugin/snmp.pp
@@ -2,20 +2,12 @@
 # $name   == the name of the plugin ( snmp__uptime )
 
 define munin::plugin::snmp (
-  $device = undef,
   $plugin,
 ){
-
-  if $device == undef {
-    $devicename = $name
-  } else {
-    $devicename = $device
-  }
-
+  $devicename  = $name
   $destination = regsubst($plugin, '__', "_${devicename}_")
 
   munin::plugin { $destination:
     fromname => $plugin
   }
-
 }
